e80a2eb199739fe02540d66814348a033588f0c1
Jason Rosario <Jasonrosario@protonmail.com>
2021-07-01 16:59:17 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+let lodash = require("lodash");
+
+let name = "jason";
+
+console.log(lodash.upperFirst(name));
+console.log(lodash.lowerCase(name));
+console.log(lodash.upperCase(name));
+console.log(lodash.startCase(name));
